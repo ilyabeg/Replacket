e6fcb0935ca346a5da683b8140b66533d7b14a0e
Ilya Beg <ilyabegichev@gmail.com>
2026-07-26 09:37:02 +0300

    Add project files.

---
+++ Replacket/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace Replacket
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
